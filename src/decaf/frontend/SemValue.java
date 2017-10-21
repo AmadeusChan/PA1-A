6e9862ac1038b7d9d7fbffcf4447b030881518d2
Amadeus Chan <greenclouds@foxmail.com>
2017-10-21 12:08:11 +0800

    modification to msg

--- src/decaf/frontend/SemValue.java
+++ src/decaf/frontend/SemValue.java
@@ -198,6 +198,33 @@ public class SemValue {
 		case Parser.STATIC:
 			msg = "keyword : static";
 			break;
+		case Parser.COMPLEX:
+			msg = "keyword : complex";
+			break;
+		case Parser.PRINTCOMP:
+			msg = "keyword : PrintComp";
+			break;
+		case Parser.CASE:
+			msg = "keyword : case";
+			break;
+		case Parser.DEFAULT:
+			msg = "keyword : default";
+			break;
+		case Parser.SUPER:
+			msg = "keyword : super";
+			break;
+		case Parser.DCOPY:
+			msg = "keyword : dcopy";
+			break;
+		case Parser.SCOPY:
+			msg = "keyword : scopy";
+			break;
+		case Parser.DO:
+			msg = "keyword : do";
+			break;
+		case Parser.OD:
+			msg = "keyword : od";
+			break;
 
 		// 常量
 		case Parser.LITERAL:
@@ -234,6 +261,9 @@ public class SemValue {
 			break;
 		case Parser.OR:
 			msg = "operator : ||";
+			break;
+		case Parser.DOSEPERATOR:
+			msg = "operator : |||";
 			break;
 		default:
 			msg = "operator : " + (char) code;
